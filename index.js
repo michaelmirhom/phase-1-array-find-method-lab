1d57c40e7b2ca3e83cb533a9c8b277b9781ce64e
michaelmirhom <micharby73@gmail.com>
2023-03-31 21:22:47 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function  superbowlWin(Array){
+    const winningYear= Array.find(Object=>Object.result==='W');
+    return winningYear? winningYear.year : undefined;
+    
+}
+/*
+function superbowlWin(array) {
+    const winningYear = array.find(obj => obj.result === "W");
+    return winningYear ? winningYear.year : undefined;
+  }*/
